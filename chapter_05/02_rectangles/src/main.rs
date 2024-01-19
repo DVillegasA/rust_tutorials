42525bd3459ffb1ece5e550739d7f55eb217aa64
Diego Villegas Arenas <diego.villegas.12@sansano.usm.cl>
2024-01-18 22:31:51 -0300

    We finished the second half of the exercises for structs from chapter 5

--- chapter_05/02_rectangles/src/main.rs
+++ chapter_05/02_rectangles/src/main.rs
@@ -12,8 +12,28 @@ impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
+
+    fn set_width(&mut self, width: u32) {
+        self.width = width;
+    }
+
+    fn max(self, other: Rectangle) -> Rectangle {
+        Rectangle {
+            width: self.width.max(other.width),
+            height: self.height.max(other.height),
+        }
+    }
+
 }
 
+impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
 
 fn main() {
     let scale = 2;
@@ -29,10 +49,28 @@ fn main() {
         width: 60,
         height: 45
     };
+    let sq = Rectangle::square(20);
 
     dbg!(&rect1);
+    dbg!(&sq);
     println!("The area of the rectangle rect1 is {} pixels", rect1.area());
 
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let mut rect = Rectangle {
+        width: 0,
+        height: 0,
+    };
+
+    rect.set_width(2);
+    println!("Area of rect: {}", rect.area());
+
+    let other_rect = Rectangle {
+        width: 1,
+        height: 1,
+    };
+
+    let max_rect = rect.max(other_rect);
+    println!("Area of max_rect: {}", max_rect.area());
 }
