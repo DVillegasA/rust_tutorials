83497b25d136f8be0b15d94217c716d62b7cb0a2
Dievo Villegas Arenas <diego.villegas.12@sansano.usm.cl>
2024-01-21 22:37:55 -0300

    First of the optional exercises from chapter 8
    It consists on finding the median and the mod of a given set of integers. I decided to generate a random vector each time the code gets execute, just to make it a little more fun.

---
+++ chapter_08/04_median_mode/src/main.rs
@@ -0,0 +1,46 @@
+use std::collections::HashMap;
+use rand::Rng;
+
+fn main() {
+    let median: i32;
+    let mut v: Vec<i32> = Vec::new();
+    
+    for _ in 0..100 {
+        v.push(rand::thread_rng().gen_range(-100..100));
+    }
+    println!("Set of data to be analize: {:?}", v);
+
+    v.sort();
+    let lenght_vector = v.len();
+    
+    // If v.len()%2 == 0 then we need the average of n/2 and n/2 + 1
+    if lenght_vector%2 == 0{
+        median = (&v[(lenght_vector/2)-1] + &v[lenght_vector/2])/2;
+    }
+    // Else is just n/2
+    else{
+        median = v[lenght_vector/2];
+    }
+
+    let mut map = HashMap::new();
+
+    // We check every element of the vector and record the amount of times each shows up
+    for i in &v {
+        let count: &mut i32 = map.entry(i).or_insert(0);
+        *count+=1;
+    }
+
+    // By finding the max value in the hashmap we know which value to look for to determine the mod
+    let mode_value = map.values().max().expect("Vector was empty somehow?");
+    let mut v2: Vec<&i32> = Vec::new();
+
+    for (key, value) in map.iter() {
+        if value == mode_value {
+            v2.push(*key);
+        }
+    }
+
+    println!("The median of the vector is {median}");
+    // println!("{:?}", map);
+    println!("The modes of the vector are {:?}", v2);
+}
