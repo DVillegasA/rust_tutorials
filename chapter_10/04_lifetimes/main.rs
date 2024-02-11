1f81570ea4266fb61f4b80f9931c2cdb7a127c21
Diego Villegas Arenas <diego.villegas.12@sansano.usm.cl>
2024-02-11 17:51:42 -0300

    Remaining exercises for lifetimes, involving their usage in structs and mixing lifetimes and generics.

--- chapter_10/04_lifetimes/main.rs
+++ chapter_10/04_lifetimes/main.rs
@@ -4,6 +4,8 @@
 &'a mut i32 -> a mutable reference with an explicit lifetime
 */
 
+use std::fmt::Display;
+
 fn lifetime_example(){
     let x = 5;            // ----------+-- 'b
                           //           |
@@ -13,8 +15,32 @@ fn lifetime_example(){
                           // --+       |
 }                         // ----------+
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str{
-    if x.len() > y.len(){
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str, 
+    y: &'a str, 
+    ann: T
+) -> &'a str 
+where 
+    T: Display 
+{
+    println!("Announcement! {}", ann);
+
+    if x.len() > y.len() {
         x
     } else {
         y
@@ -26,7 +52,16 @@ fn main(){
     let string1 = String::from("long string is long");
     {
         let string2 = "xyz";
-        let result = longest(string1.as_str(), string2);
+        let result = longest_with_an_announcement(string1.as_str(), string2, "THIS IS IMPORTANT!!!");
         println!("The longest string is {}", result);
     }
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Couldn't find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+    let part: &str = i.announce_and_return_part("This is important");
+    println!("Important excerpt: {:?}", part);
+    println!("Current level: {}", i.level());
 }
